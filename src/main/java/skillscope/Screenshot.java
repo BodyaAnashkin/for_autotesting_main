39eaf4ec536c40e49d3836714f4f10877ca5cf75
Bohdan <95252447+BodyaAnashkin@users.noreply.github.com>
2023-05-13 21:49:07 +0300

    Add xml file
    add xml file for start collection tests

--- src/main/java/skillscope/Screenshot.java
+++ src/main/java/skillscope/Screenshot.java
@@ -7,14 +7,12 @@ import org.openqa.selenium.WebDriver;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class Screenshot  {
     private WebDriver driver;
 
     public void screenshot(WebDriver driver, String nameScreen) throws IOException, InterruptedException {
         this.driver = driver;
-        driver.manage().timeouts().implicitlyWait( 10, TimeUnit.SECONDS );
         Thread.sleep(2000);
         // создаем объект класса TakesScreenshot
         TakesScreenshot screenshot = (TakesScreenshot) driver;
@@ -27,7 +25,6 @@ public class Screenshot  {
 
         // копируем скриншот в указанный файл
         FileUtils.copyFile(srcFile, new File(filePath));
-
 
     }
 
